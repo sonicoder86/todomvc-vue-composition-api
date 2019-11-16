36a22806c6db158a971df2b45637cc3310a1f61e
blacksonic <soos.gabor86@gmail.com>
2019-11-16 19:57:20 +0100

    feat(package): upgrade deps and fix linting

--- src/components/CopyRight.spec.js
+++ src/components/CopyRight.spec.js
@@ -1,8 +1,11 @@
 import CopyRightComponent from './CopyRight.vue';
 import { shallowMount } from '@vue/test-utils';
+import { expect } from 'chai';
 
 describe('CopyRight', function() {
-  it('should be created', function() {
-    const wrapper = shallowMount(CopyRightComponent)
+  it('should contain creator and instruction', function() {
+    const wrapper = shallowMount(CopyRightComponent);
+
+    expect(wrapper.find('.info').text()).to.eql('Double-click to edit a todoCreated by blacksonicPart of TodoMVC');
   });
 });
