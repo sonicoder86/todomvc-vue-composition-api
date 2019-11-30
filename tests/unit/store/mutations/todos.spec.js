05590ac11e35d7c7697d9eb593e1f484f2df22dd
blacksonic <soos.gabor86@gmail.com>
2019-11-30 18:30:55 +0100

    feat(test): more mutation tests

--- tests/unit/store/mutations/todos.spec.js
+++ tests/unit/store/mutations/todos.spec.js
@@ -3,13 +3,40 @@ import { todosMutations } from '@/store/mutations/todos';
 
 describe('todosMutations', function() {
   it('should set list of items on load', function() {
-    const state = {};
-    const todos = [{ id: 'e2bb892a-844a-47fb-a2b3-47f491af9d88', name: 'Demo', completed: false }];
+    const state = { todos: [] };
+    const todos = [{ id: 'e2bb892a', name: 'Demo', completed: false }];
 
     todosMutations.load(state, { todos });
 
     expect(state.todos).to.eql(todos);
     expect(state.todos).to.have.length(1);
     expect(state.todos).to.contain(todos[0]);
+  });
+
+  it('should create new todo', function() {
+    const state = { todos: [] };
+
+    todosMutations.create(state, { name: 'Demo' });
+
+    expect(state.todos).to.have.length(1);
+    expect(state.todos[0].id).to.be.a('string');
+    expect(state.todos[0].name).to.eql('Demo');
+    expect(state.todos[0].completed).to.eql(false);
+  });
+
+  it('should update existing todo', function() {
+    const state = { todos: [{ id: 'e2bb892a', name: 'Demo', completed: false }] };
+
+    todosMutations.update(state, { id: 'e2bb892a', values: { name: 'Demo2' } });
+
+    expect(state.todos[0].name).to.eql('Demo2');
+  });
+
+  it('should remove existing todo', function() {
+    const state = { todos: [{ id: 'e2bb892a', name: 'Demo', completed: false }] };
+
+    todosMutations.remove(state, { id: 'e2bb892a' });
+
+    expect(state.todos).to.have.length(0);
   });
 });
