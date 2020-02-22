868e6a77f4838612971e129ac57b39eeaafcd248
blacksonic <soos.gabor86@gmail.com>
2020-02-22 23:37:35 +0100

    fix(test): fix header test

--- tests/unit/components/Header.spec.js
+++ tests/unit/components/Header.spec.js
@@ -13,10 +13,13 @@ describe('Header', () => {
     const wrapper = shallowMount(Header, { localVue, store });
 
     const input = wrapper.find('input');
-    input.element.value = 'New';
+    input.element.value = 'Demo';
     input.trigger('input');
-    input.trigger('keyup.enter');
+    input.trigger('keyup', { key: 'Enter' });
 
-    expect(store.state.todos).to.eql([]);
+    expect(store.state.todos).to.have.length(1);
+    expect(store.state.todos[0].id).to.be.a('string');
+    expect(store.state.todos[0].name).to.eql('Demo');
+    expect(store.state.todos[0].completed).to.eql(false);
   });
 });
