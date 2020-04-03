1854513f8db4f14e57435f82d7884efff5692578
blacksonic <soos.gabor86@gmail.com>
2020-04-03 20:40:56 +0200

    fix(package): upgrade deps

--- src/store/mutations/todo.js
+++ src/store/mutations/todo.js
@@ -1,12 +1,12 @@
 import { ACTION_TYPES } from '../../constants/action-types';
-import uuid from 'uuid/v4';
+import { v4 as uuidv4 } from 'uuid';
 import { selectCompleted, selectNotCompleted } from '../getters/todo';
 
 export const todosMutations = {
   [ACTION_TYPES.load]: (state, { todos }) =>
     state.todos = todos,
   [ACTION_TYPES.create]: (state, { name }) =>
-    state.todos = [...state.todos, { id: uuid(), name, completed: false }],
+    state.todos = [...state.todos, { id: uuidv4(), name, completed: false }],
   [ACTION_TYPES.update]: (state, values) =>
     state.todos = state.todos.map(
       todo => todo.id === values.id ? { ...todo, ...values } : todo
