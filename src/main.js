97497073e146b4d140f8a9a9baf59fb93bb0badf
blacksonic <soos.gabor86@gmail.com>
2020-04-05 18:52:35 +0200

    feat(core): upgrade to vue 3

--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import { createApp } from 'vue';
 import App from './components/app/app.vue';
 import 'todomvc-app-css/index.css';
 import { createStore } from './store/index';
 
-Vue.config.productionTip = false;
-Vue.use(Vuex);
-
-new Vue({
-  store: createStore(),
-  render: h => h(App)
-}).$mount('app-root');
+const app = createApp(App);
+app.use(createStore());
+app.mount('app-root');
