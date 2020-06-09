5f4e34e20e28fdb5918cd1ce5bda5eee971e9c23
blacksonic <soos.gabor86@gmail.com>
2020-06-09 08:43:10 +0200

    fix(package): upgrade deps

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -35,7 +35,7 @@ export default {
     >
     <ul class="filters">
       <li v-for="filterTitle in filterTitles" :key="filterTitle.key">
-        <a href="#" :class="{ selected: filterTitle.key === filter }" @click="onFilterSelect(filterTitle.key)">
+        <a href="./#" :class="{ selected: filterTitle.key === filter }" @click="onFilterSelect(filterTitle.key)">
           {{ filterTitle.value }}
         </a>
       </li>
