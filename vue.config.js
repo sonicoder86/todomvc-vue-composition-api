8e21305042893396bb10ea317ef638a6174c864a
blacksonic <soos.gabor86@gmail.com>
2020-06-23 15:11:45 +0200

    feat(wc): try to make compiler options work

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,14 @@ module.exports = {
   devServer: {
     port: 9000
   }
+  // ,
+  // chainWebpack: config => {
+  //   // get the existing vue-loader rule and tap into its options
+  //   config.module.rule('vue-loader').tap(options => {
+  //     options.compilerOptions = {
+  //       ...(options.compilerOptions || {}), // merge existing compilerOptions, if any
+  //       isCustomElement: tag => /^x-/.test(tag)
+  //     }
+  //   })
+  // }
 };
